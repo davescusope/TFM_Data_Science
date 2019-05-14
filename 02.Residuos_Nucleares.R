09bc53640d876f043ad9f42c791c9ef16f04d312
davescusope <46086706+davescusope@users.noreply.github.com>
2019-05-14 18:25:30 +0200

    Add files via upload

--- 02.Residuos_Nucleares.R
+++ 02.Residuos_Nucleares.R
@@ -1,3 +1,14 @@
+# Instruction to measure how much time does it take in my machine   <<<<<Time difference of 6.325827 secs>>>>>>
+Start_Time <- Sys.time()
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
 list.of.packages <- c("dplyr", "tidyverse", "ggplot2", "tidyr", 'lubridate','viridis','R.utils','xlsx' )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -32,17 +43,40 @@ Nuclear_Wastes <- Import_Nuclear_Wastes %>%
   filter(VALOR!=0) %>%
   filter(ID_UNIDAD == "EUROS") %>%
   rename(ID_CONCEPTO = ID_CUENTA) %>% 
-  rename(ID_UPR = ID_NODO) %>% 
+  rename(ID_UPR = ID_NODO) %>%
+  rename(ID_CONCEPTO_CTRL = ID_CONCEPTO) %>% 
+  rename(VERSION = FC_PERIODO) %>% 
+  mutate(VERSION = as.Date(VERSION, format = "%d/%m/%Y")) %>% 
+  mutate(VERSION = format(as.Date(VERSION), "%Y%m")) %>% 
   mutate(ID_GRUPO_EMPRESARIAL ="EMPRESA1") %>%
   mutate(ID_AREA_SISTEMA = 'ESPAÑA') %>% 
-  mutate(ID_TECNOLOGIA ="NC") 
+  mutate(ID_TECNOLOGIA ="NC") %>% 
+  mutate(VALOR = format(VALOR, decimal.mark=",")) %>% 
+  arrange(VERSION)
 Nuclear_Wastes <-  Nuclear_Wastes[c(1,2,8,6,7,3,5,4)]
 
 
-write.table(Nuclear_Wastes, 
+write.table(format(Nuclear_Wastes, drop0trailing=FALSE), 
             file = str_c(getwd(),"/Outputs/NUCLEAR_WASTES_EUROS.csv", sep = "", collapse = NULL),
             row.names=FALSE, 
             sep = ";")
 
-rm(Import_Nuclear_Wastes)
+
+
+
+End_Time <- Sys.time()
+Duration_Process <- End_Time - Start_Time
+Duration_Process
+
+rm(Import_Nuclear_Wastes,End_Time,Start_Time,Duration_Process)
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
 
